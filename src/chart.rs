4f659a118285ef2dd13570a7ffb41bfcaec88896
Rai T <53047500+bowl-of-rais@users.noreply.github.com>
2024-02-17 17:36:05 +0100

    displaying chart

--- src/chart.rs
+++ src/chart.rs
@@ -1,10 +1,12 @@
+use std::fmt;
+use std::fmt::Display;
+
 use array2d::Array2D;
 
-static CHART_SIZE: usize = 11;
+const CHART_SIZE: usize = 11;
 
 #[derive(Clone)]
 pub enum Terrain {
-    Borderland,
     Empty,
     Farm,
     Monster,
@@ -15,10 +17,39 @@ pub enum Terrain {
     Water,
 }
 
+impl Display for Terrain {
+    fn fmt(&self, f: &mut std::fmt::Formatter)->std::fmt::Result{
+        match self {
+            Terrain::Empty => Display::fmt(&" _ ", f),
+            Terrain::Farm =>  Display::fmt(&" F ", f),
+            Terrain::Monster => Display::fmt(&" M ", f),
+            Terrain::Mountain =>  Display::fmt(&" ^ ", f),
+            Terrain::Trees =>  Display::fmt(&" T ", f),
+            Terrain::Village =>  Display::fmt(&" V ", f),
+            Terrain::Wasteland =>  Display::fmt(&" * ", f),
+            Terrain::Water =>  Display::fmt(&" W ", f),
+        }
+    }
+}
+
 pub struct Chart {
     contents: Array2D<Terrain>,
     ruins: Vec<(usize, usize)>,
     penalized: Vec<(usize, usize)>
+}
+
+impl Display for Chart {
+    fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
+        for row_iter in self.contents.rows_iter() {
+            Display::fmt(&'|', f)?;
+            for element in row_iter {
+                Display::fmt(element, f)?;
+            }
+            Display::fmt(&'|', f)?;
+            Display::fmt(&'\n', f)?;
+        }
+        Ok(())
+    }
 }
 
 impl Chart {
