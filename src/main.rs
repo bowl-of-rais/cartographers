12541f4da05624c66c2de73190bca974bc7730b1
Rai T <53047500+bowl-of-rais@users.noreply.github.com>
2024-02-18 13:08:17 +0100

    reading explorations works :)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod chart;
+mod exploration;
 
 use chart::Chart;
 
@@ -6,6 +7,14 @@ fn main() {
     let mut c: Chart = Chart::new();
     match c.set(3, 3, chart::Terrain::Trees) {
         Ok(_) => print!("{}", c),
+        Err(e) => print!("{}", e)
+    }
+
+    let path = "assets/explorations.json";
+    let expls = exploration::read_explorations(path);
+
+    match expls {
+        Ok(explorations) => for ex in explorations { print!("{}", ex); },
         Err(e) => print!("{}", e)
     }
 }
