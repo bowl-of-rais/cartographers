1f9ac00eed5d574d0949291fb0627911e3d225c9
Rai T <53047500+bowl-of-rais@users.noreply.github.com>
2024-02-19 14:33:11 +0100

    fixed Player

--- src/player.rs
+++ src/player.rs
@@ -3,12 +3,11 @@ use crate::chart::Chart;
 pub struct Player {
     coins : i8,
     points : [i8;4],
-    map : Map,
     chart : Chart,
 }
 
 impl Player {
-    fn new(num_mountains: i8, num_ruins: i8, num_wastelands: i8) -> Player {
+    fn new() -> Player {
         Player {
             coins: 0,
             points: [0, 0, 0, 0],
