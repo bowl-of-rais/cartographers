718b1d3b53680c25ff2944edc2654ae30ee59935
Rai Trouvain <raitrouvain@gmail.com>
2024-05-29 23:29:38 +0200

    Read for Edicts + thinking about functions

--- src/edicts.rs
+++ src/edicts.rs
@@ -1,7 +1,9 @@
-use crate::chart::Terrain;
+use std::{error::Error, fs};
+use serde::Deserialize;
+use crate::{chart::Chart, chart::Terrain, resource::Read};
 
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Deserialize)]
 enum Segment {
     Row,
     Column,
@@ -12,6 +14,15 @@ enum Segment {
     Square(usize)
 }
 
+#[derive(Deserialize)]
+enum Category {
+    A,
+    B, 
+    C,
+    D
+}
+
+#[derive(Deserialize)]
 enum ScoreCondition {
     Containing(usize, Terrain), // TODO: min, max, only, not, different terrains?
     In(Segment, Terrain),
@@ -19,11 +30,15 @@ enum ScoreCondition {
     DifferentTerrains(usize)
 }
 
+#[derive(Deserialize)]
 pub struct Edict {
     multiplier : usize,
     scored_segment : Segment,
-    condition : ScoreCondition
+    condition : ScoreCondition,
+    category : Category
 }
+
+const EDICTS_PATH : &str = "assets/edicts.json";
 
 impl Default for Edict {
     fn default() -> Self {
@@ -31,8 +46,27 @@ impl Default for Edict {
             multiplier : 0,
             scored_segment : Segment::Row,
             condition : ScoreCondition::Containing(1, Terrain::Empty), 
+            category : Category::A
         }
     }
 }
 
-// TODO: read edicts from json
+impl Read for Edict {
+    fn read() -> Result<Vec<Self>, Box<dyn Error>> {
+        let file_contents = fs::read_to_string(EDICTS_PATH)?;
+        let edicts : Vec<Edict> = serde_json::from_str(&file_contents)?;
+        Ok(edicts)
+    }
+}
+
+impl Edict {
+    pub fn draw() -> [Self; 4] {
+        !todo!()
+    }
+
+    pub fn score(chart: &Chart) -> i8 {
+        !todo!()
+    }
+}
+
+// TODO: serialize edicts T-T
